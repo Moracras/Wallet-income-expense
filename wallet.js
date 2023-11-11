51247034ecedd8938458e00e633556c1a050465d
Moracras <tavfikkarademir@gmail.com>
2023-11-12 02:03:29 +0300

    :(

--- wallet.js
+++ wallet.js
@@ -2,12 +2,14 @@ let inputdate = document.querySelector("#inputdate")
 
 let inputSC = document.querySelector("#inputSC")
 
-let inputA = document.querySelector("#amountCurrency")
+let inputA = document.querySelector("#amountCurrency").value
 let inputbtn = document.querySelector("#inputbtn")
 
 let showSpending = document.querySelector("#showSpending")
 let expense = document.querySelector("#expense")
 
+const exchangeRatesToLira ={USD:28.55,EURO:30.51,GBP:34.90,KWD:92.09,Gold:1776.88
+}
 
 
 
@@ -45,26 +47,63 @@ const show = () =>{
 const selectedCT= ()=>{
     const selectCT =document.getElementById("selectOptions")
      return selectCT.options[selectCT.selectedIndex].value
-    
 }
+
 const selectedIncOrExp = () =>{
     const selectIoE = document.getElementById("selectIoE")
     return selectIoE.options[selectIoE.selectedIndex].value
 }
+const convertingExchange = (exchangeRatesToLira,selectedCT,inputA)=>{   
+    switch (selectedCT){
+        case 'USD':
+            return    exchangeRatesToLira['USD']*inputA;
+            
+        case 'EURO':
+            return   exchangeRatesToLira['EURO']*inputA;
+
+        case 'GBP':
+             return  exchangeRatesToLira['GBP']*inputA;
+            
+        case 'KWD':
+            return  exchangeRatesToLira['KWD']*inputA;
+            
+        case 'Gold':
+            return  exchangeRatesToLira['Gold']*inputA;
+            
+        default:
+            return inputA
+    }
+  }
 
 
 const getIncomeExpense = () => {
     let getData = getStorage();
+    let totalIncome = 0
+    let totalExpense = 0
+    getData.forEach(item => {
+        if(item.ioe ==="Income"){
+            const convertedMoney=convertingExchange(exchangeRatesToLira,item.ct,item.ia)
+            totalIncome += parseFloat(convertedMoney)
+            console.log(totalIncome);
+            
+        }else if (item.ioe ==="Expense"){
+            const convertedMoney=convertingExchange(exchangeRatesToLira,item.ct,item.ia)
+            totalExpense += parseFloat(convertedMoney)
+            console.log(totalExpense);
+        }
+    })
+    
     return getData.map(item => parseInt(item.ia) || 0).reduce((sum, item) => sum + item, 0);
-  }
+}
 inputbtn.addEventListener("click", (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+
     let newData = {
         date: inputdate.value,
         ioe:selectedIncOrExp(),
         desc: inputSC.value,
         ct: selectedCT(),
-        ia: inputA.value,
+        ia: inputA,
     };
 
     data.push(newData);
